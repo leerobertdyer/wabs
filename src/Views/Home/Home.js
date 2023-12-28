e9d429bf37222798004ccfa270d3a143ba208a99
leerobertdyer <leerobertdyer@gmail.com>
2023-12-28 14:06:46 -0500

    adjusting copy

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -6,19 +6,19 @@ const Home = () => {
     <div className='mainHomeDiv'>
       <div className='homeTopImage'>
         <div className='headerTextDiv'>
-          <p className='firstLine'>Not everyone is Mozart</p>
+          <p className='firstLine'>Not everyone is Mozart...</p>
           <p className='headerText'>Some of us need to</p>
           <p className='headerText'><span className='writeabadsong'>Write a <span className='bad'>bad </span>song</span> first.</p>
         </div>
       </div>
       <div className='secondDiv'>
-        <p className='headerText center'>Some of us need a little push</p>
-        <p className='headerText center'>So we award points each month</p>
-        <p className='headerText center'>For those who post early and often</p>
+        <p className='headerText center'>Writing is a muscle...</p>
+        <p className='headerText center'>And bad songs are the weights</p>
+        <p className='headerText center'>That create metaphorical six packs</p>
       </div>
       <div className='thirdDiv'>
-        <p className='headerText center'>Trouble finishing songs?</p>
-        <p className='headerText center'>Try our collab page!</p>
+        <p className='headerText center'>Have trouble finishing songs?</p>
+        <p className='headerText center'>Try out the collab page!</p>
         <Link to='/collaborate' className="center linkBtn">
         <button className='btn center'>Collaborate!</button>
         </Link>
