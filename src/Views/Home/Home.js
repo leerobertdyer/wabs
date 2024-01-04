140dd77e85b9211c4fef800ca11ee7785982834a
leerobertdyer <leerobertdyer@gmail.com>
2024-01-04 15:53:48 -0500

    homepage

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -16,26 +16,26 @@ const Home = () => {
           </Link>}
       </div>
       <div className='secondDiv'>
-        <p className='headerText center'>Writing is a muscle...</p>
-        <p className='headerText center'>And bad songs are the weights</p>
-        <p className='headerText center'>That create metaphorical six packs</p>
+        <p className='headerText center '>Writing is a muscle...</p>
+        <p className='headerText center '>And bad songs are the weights</p>
+        <p className='headerText center '>That create metaphorical six packs</p>
       </div>
       <div className='thirdDiv'>
-        <p className='headerText center'>Have trouble finishing songs?</p>
-        <p className='headerText center'>Try out the collab page!</p>
-        <Link to='/collaborate' className="center linkBtn">
+        <p className='headerText center '>Have trouble finishing songs?</p>
+        <p className='headerText center '>Try out the collab page!</p>
+        <Link to='/collaborate' className="center linkBtn ">
         <button className='btn center'>Collaborate!</button>
         </Link>
       </div>
       <div className='secondDiv'>
-        <p className='headerText center'>Just want to see what's here?</p>
-        <p className='headerText center'>Check out the feed!</p>
+        <p className='headerText center '>Just want to see what's here?</p>
+        <p className='headerText center '>Check out the feed!</p>
         <Link to='/feed' className="center linkBtn">
           <button className='btn center'>Scroll Away!</button>
         </Link>
       </div>
       <div className='thirdDiv'>
-        <p className='headerText center'>Or are you ready to dive in?</p>
+        <p className='headerText center '>Or are you ready to dive in?</p>
         <Link to='/profile' className="center linkBtn">
         <button className='btn center'>Profile</button>
         </Link>
